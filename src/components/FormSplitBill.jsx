97f5391cb8f664974e7f070b9c986cd6260f12d9
GarryAmasia <amasiagarry87@gmail.com>
2023-12-09 20:41:07 +1100

    adding controlled element for split bill form

--- src/components/FormSplitBill.jsx
+++ src/components/FormSplitBill.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../Button";
 
 export const FormSplitBill = ({ selectedFriend }) => {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
   const { name } = selectedFriend;
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {name}</h2>
       <label>💰 Bill Value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(+e.target.value)}
+      />
       <label>😱 Your expenses</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(+e.target.value > bill ? paidByUser : +e.target.value)
+        }
+      />
       <label>💲 {name}'s expenses</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
       <label>💸 Whos paying the bill</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">me</option>
         <option value="friend">{name}</option>
       </select>
